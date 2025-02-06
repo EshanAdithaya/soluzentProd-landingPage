05a6bf149bd91b8706b590f9f5cb41e5419614ee
S. D. E. A. Gunathilaka <87906517+EshanAdithaya@users.noreply.github.com>
2025-02-06 09:26:12 +0530

    remove ProjectShowcase component and related routes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import Lander from './Lander';
 import PremiumLexWebsite from './PremiumLexWebsite';
-import ProjectShowcase from './ProjectShowcase';
-import AboutUs from './AboutUs';
+
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<PremiumLexWebsite />} />
-        <Route path="/projects" element={<ProjectShowcase />} />
-        <Route path="/aboutus" element={<AboutUs />} />
       </Routes>
     </Router>
   );
